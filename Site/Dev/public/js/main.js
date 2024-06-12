467246f222f26947fbeae8d70a748cdd109270b9
petarnikolic <petar147nikolic@gmail.com>
2024-06-12 15:08:00 +0200

    aded city to marketing data, changed the timestamp format.

--- Site/Dev/public/js/main.js
+++ Site/Dev/public/js/main.js
@@ -1,239 +1,242 @@
-let currentUser = null
-let userId = null
-let unity = null
-let isBreakingScript = false
-const dinamicParam = new URLSearchParams(window.location.search).get("a")
-const btnCancelPayment = document.querySelector("#btn-cancelPayment")
+let currentUser = null;
+let userId = null;
+let unity = null;
+let isBreakingScript = false;
+const dinamicParam = new URLSearchParams(window.location.search).get("a");
+const btnCancelPayment = document.querySelector("#btn-cancelPayment");
 const stripe = Stripe(
   "pk_live_51O9azOCJmlrAlGBoAojhLshRHV3rr5qqUXrdG13NYxbpkfLU69WVgypk06ZiARta7y7LSA3wEhbJNf2K6CvWP5Wf00trRX4rYy"
-)
+);
 
 document.addEventListener("unityInitialized", function () {
-  CheckUserSession()
-})
+  CheckUserSession();
+});
 
 function CheckHasBoundAccount() {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      let isBounded = !user.isAnonymous
+      let isBounded = !user.isAnonymous;
       unity.SendMessage(
         "JavaScriptManager",
         "HasBoundedAccount",
         JSON.stringify({ IsBounded: isBounded })
-      )
+      );
     }
-  })
+  });
 }
 
 function IsAndroid() {
-  return /Android/.test(navigator.userAgent)
+  return /Android/.test(navigator.userAgent);
 }
 
 function CheckIsPwa() {
   function IsInStandaloneMode() {
-    var displayModes = ["fullscreen", "standalone", "minimal-ui"]
+    var displayModes = ["fullscreen", "standalone", "minimal-ui"];
     return displayModes.some(
       (displayMode) =>
         window.matchMedia("(display-mode: " + displayMode + ")").matches
-    )
+    );
   }
 
-  const isIOS = /iPhone|iPad|iPod/.test(navigator.userAgent)
-  const isAndroid = /Android/.test(navigator.userAgent)
-  const isOnPC = !isIOS && !isAndroid
+  const isIOS = /iPhone|iPad|iPod/.test(navigator.userAgent);
+  const isAndroid = /Android/.test(navigator.userAgent);
+  const isOnPC = !isIOS && !isAndroid;
 
-  let res
+  let res;
   if (!isOnPC && IsInStandaloneMode()) {
-    res = true // is PWA
+    res = true; // is PWA
   } else {
-    res = false // is not PWA
+    res = false; // is not PWA
   }
 
-  return res
+  return res;
 }
 
 function IsOnPc() {
-  const isIOS = /iPhone|iPad|iPod/.test(navigator.userAgent)
-  const isAndroid = /Android/.test(navigator.userAgent)
-  const isOnPC = !isIOS && !isAndroid
+  const isIOS = /iPhone|iPad|iPod/.test(navigator.userAgent);
+  const isAndroid = /Android/.test(navigator.userAgent);
+  const isOnPC = !isIOS && !isAndroid;
 
-  return isOnPC
+  return isOnPC;
 }
 
 function SetUserId(id) {
-  userId = id
+  userId = id;
 
   // Reference to the user's 'Coins' field in Firebase
-  var coinsRef = firebase.database().ref(`users/${userId}/Coins`)
+  var coinsRef = firebase.database().ref(`users/${userId}/Coins`);
   // Listener for changes in the 'Coins' field
   coinsRef.on("value", function (snapshot) {
     if (snapshot.exists()) {
-      const coins = snapshot.val()
+      const coins = snapshot.val();
       //unity.SendMessage('JavaScriptManager', 'OnUpdatedDiamonds', coins.toString());
     }
-  })
+  });
 
   // Reference to the user's 'USDC' field in Firebase
-  var usdcRef = firebase.database().ref(`users/${userId}/USDC`)
+  var usdcRef = firebase.database().ref(`users/${userId}/USDC`);
   // Listener for changes in the 'USDC' field
   usdcRef.on("value", function (snapshot) {
     if (snapshot.exists()) {
-      const usdc = snapshot.val()
-      unity.SendMessage("JavaScriptManager", "OnUpdatedUSDC", usdc.toString())
+      const usdc = snapshot.val();
+      unity.SendMessage("JavaScriptManager", "OnUpdatedUSDC", usdc.toString());
     }
-  })
+  });
 }
 
 async function updateMarketingParam(param, userId) {
   if (!param) {
-    console.log("No dynamicParam found in URL")
-    return
+    console.log("No dynamicParam found in URL");
+    return;
   }
 
-  let _randomId = crypto.randomUUID()
-  let countryName = null
-  const _key = "uniqueIdForMarketing"
-  let _idFromStorage = localStorage.getItem(_key)
-  let _hadId = _idFromStorage != null
-  let _updateDevices = false
+  let _randomId = crypto.randomUUID();
+  let countryName = null;
+  let cityName = null;
+  const _key = "uniqueIdForMarketing";
+  let _idFromStorage = localStorage.getItem(_key);
+  let _hadId = _idFromStorage != null;
+  let _updateDevices = false;
   if (_hadId) {
-    _randomId = _idFromStorage
+    _randomId = _idFromStorage;
   } else {
-    localStorage.setItem(_key, _randomId)
-    _updateDevices = true
+    localStorage.setItem(_key, _randomId);
+    _updateDevices = true;
   }
 
-  console.log("Found agency: " + param)
-  const agencyRef = firebase.database().ref(`marketing/${param}/click`)
+  console.log("Found agency: " + param);
+  const agencyRef = firebase.database().ref(`marketing/${param}/click`);
   agencyRef.once("value", async function (snapshot) {
-    console.log("Got value")
+    console.log("Got value");
 
     try {
-      const geoData = await getGeolocation()
-      countryName = geoData.country_name
-
+      const geoData = await getGeolocation();
+      countryName = geoData.country_name;
+      cityName = geoData.city;
       agencyRef.push(
         {
-          timestamp: firebase.database.ServerValue.TIMESTAMP,
+          timestamp: new Date().toISOString(),
           deviceId: _randomId,
           country: countryName,
+          city: cityName,
         },
         function (error) {
           if (error) {
-            console.error("Error updating marketing user:", error)
+            console.error("Error updating marketing user:", error);
           } else {
             console.log(
               `User '${userId}' added to agency '${param}' with timestamp and country.`
-            )
+            );
           }
         }
-      )
+      );
       if (!_updateDevices) {
-        return
+        return;
       }
 
-      await SaveAgencyUniqueDevice(_randomId, countryName)
+      await SaveAgencyUniqueDevice(_randomId, countryName, cityName);
     } catch (error) {
       console.error(
         "Failed to get geolocation data for marketing param:",
         error
-      )
+      );
     }
-  })
+  });
 }
 
-async function SaveAgencyUniqueDevice(deviceId, countryName) {
+async function SaveAgencyUniqueDevice(deviceId, countryName, cityName) {
   if (
     dinamicParam === null ||
     dinamicParam === undefined ||
     dinamicParam === ""
   ) {
-    return
+    return;
   }
-  console.log("Got device id: " + deviceId)
+  console.log("Got device id: " + deviceId);
   const agencyRef = firebase
     .database()
-    .ref(`marketing/${dinamicParam}/devices/${deviceId}`)
+    .ref(`marketing/${dinamicParam}/devices/${deviceId}`);
   agencyRef.once("value", function (snapshot) {
-    console.log("Got value")
+    console.log("Got value");
     agencyRef.set(
       {
-        timestamp: firebase.database.ServerValue.TIMESTAMP,
+        timestamp: new Date().toISOString(),
         country: countryName,
+        city: cityName,
       },
       function (error) {
         if (error) {
-          console.error("Error updating marketing user:", error)
+          console.error("Error updating marketing user:", error);
         } else {
           console.log(
             `User '${userId}' added to agency '${dinamicParam}' with timestamp.`
-          )
+          );
         }
       }
-    )
-  })
+    );
+  });
 }
 
-updateMarketingParam(dinamicParam)
+updateMarketingParam(dinamicParam);
 
 const ReloadPageFromUnity = () => {
-  location.reload()
-}
+  location.reload();
+};
 
 const CheckUserSession = () => {
-  console.log("Checking session")
+  console.log("Checking session");
   if (!unity) {
-    console.log("Unity instance is not ready.")
-    return // Exit the function if unity is not initialized
+    console.log("Unity instance is not ready.");
+    return; // Exit the function if unity is not initialized
   }
   if (currentUser) {
-    tellUnityUserInfo(currentUser.uid, currentUser.isAnonymous, currentUser)
+    tellUnityUserInfo(currentUser.uid, currentUser.isAnonymous, currentUser);
   } else {
-    unity.SendMessage("JavaScriptManager", "AuthFinished", "")
+    unity.SendMessage("JavaScriptManager", "AuthFinished", "");
   }
-}
+};
 
 function LinkingAnonimousUser(providerName) {
-  var user = firebase.auth().currentUser
-  if (!user) return //if user doesn't exsist that means that client is not logged in
-  let provider = null
+  var user = firebase.auth().currentUser;
+  if (!user) return; //if user doesn't exsist that means that client is not logged in
+  let provider = null;
   switch (
     providerName //choose a provider based on string that is provided by specific button from unity
   ) {
     case "google":
-      provider = googleProvider
-      break
+      provider = googleProvider;
+      break;
     case "facebook":
-      provider = facebookProvider
-      break
+      provider = facebookProvider;
+      break;
     case "twitter":
-      provider = twitterProvider
-      break
+      provider = twitterProvider;
+      break;
     default:
-      console.log("Unsupported provider")
-      return
+      console.log("Unsupported provider");
+      return;
   }
   user
     .linkWithPopup(provider)
     .then((result) => {
       //call sign up with specific provider
-      var credential = result.credential
-      var user = result.user
-      unity.SendMessage("JavaScriptManager", "SuccessLinkingLoginAccount")
+      var credential = result.credential;
+      var user = result.user;
+      unity.SendMessage("JavaScriptManager", "SuccessLinkingLoginAccount");
     })
     .catch((error) => {
-      console.log(error)
-      unity.SendMessage("JavaScriptManager", "UserAlreadyHasAccount")
-    })
+      console.log(error);
+      unity.SendMessage("JavaScriptManager", "UserAlreadyHasAccount");
+    });
 }
 
 function tellUnityUserInfo(userId, isGuest, userInfo) {
-  var ref = firebase.database().ref(`users/${userId}`)
+  var ref = firebase.database().ref(`users/${userId}`);
   ref
     .once("value", function (snapshot) {
       if (snapshot.exists()) {
-        const userData = snapshot.val()
-        console.log("User Data Retrieved:", userData)
+        const userData = snapshot.val();
+        console.log("User Data Retrieved:", userData);
 
         unity.SendMessage(
           "JavaScriptManager",
@@ -244,9 +247,9 @@ function tellUnityUserInfo(userId, isGuest, userInfo) {
             Agency: dinamicParam,
             IsGuest: isGuest,
           })
-        )
+        );
       } else {
-        console.log("No user data available for user ID:", userId)
+        console.log("No user data available for user ID:", userId);
 
         unity.SendMessage(
           "JavaScriptManager",
@@ -257,26 +260,26 @@ function tellUnityUserInfo(userId, isGuest, userInfo) {
             Agency: dinamicParam,
             IsGuest: isGuest,
           })
-        )
+        );
       }
     })
     .catch(function (error) {
-      console.error("Error fetching user data:", error)
-      unity.SendMessage("JavaScriptManager", "FailedToAuth")
-    })
+      console.error("Error fetching user data:", error);
+      unity.SendMessage("JavaScriptManager", "FailedToAuth");
+    });
 }
 
 function checkAndReauthenticateUser() {
-  const userAuthDataString = localStorage.getItem("userReauthenticate")
+  const userAuthDataString = localStorage.getItem("userReauthenticate");
   if (userAuthDataString) {
-    const userAuthData = JSON.parse(userAuthDataString)
-    const { uid, token, provider } = userAuthData
+    const userAuthData = JSON.parse(userAuthDataString);
+    const { uid, token, provider } = userAuthData;
 
-    let credential
+    let credential;
     if (provider === "google") {
-      credential = firebase.auth.GoogleAuthProvider.credential(null, token)
+      credential = firebase.auth.GoogleAuthProvider.credential(null, token);
     } else if (provider === "twitter") {
-      credential = firebase.auth.TwitterAuthProvider.credential(token)
+      credential = firebase.auth.TwitterAuthProvider.credential(token);
     }
 
     if (credential) {
@@ -284,33 +287,33 @@ function checkAndReauthenticateUser() {
         .auth()
         .signInWithCredential(credential)
         .then((userCredential) => {
-          console.log("User re-authenticated:", userCredential)
+          console.log("User re-authenticated:", userCredential);
 
           tellUnityUserInfo(
             userCredential.user.uid,
             userCredential.user.isAnonymous,
             userCredential.user
-          )
+          );
         })
         .catch((error) => {
-          console.error("Re-authentication failed:", error)
-          localStorage.removeItem("userReauthenticate")
-        })
+          console.error("Re-authentication failed:", error);
+          localStorage.removeItem("userReauthenticate");
+        });
     }
   }
 }
 
-checkAndReauthenticateUser()
+checkAndReauthenticateUser();
 
 const SignOut = () => {
   firebase
     .auth()
     .signOut()
     .then(() => {
-      localStorage.removeItem("userReauthenticate")
-      ReloadPageFromUnity()
+      localStorage.removeItem("userReauthenticate");
+      ReloadPageFromUnity();
     })
     .catch((error) => {
-      console.log("greska")
-    })
-}
+      console.log("greska");
+    });
+};
