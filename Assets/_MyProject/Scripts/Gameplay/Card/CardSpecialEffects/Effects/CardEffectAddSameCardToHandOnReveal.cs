03044ffa91d1936d93fa388832a2468bb76be9ca
Filip <filpkrcmarevic555@gmail.com>
2024-06-13 14:59:54 +0200

    Added logs

--- Assets/_MyProject/Scripts/Gameplay/Card/CardSpecialEffects/Effects/CardEffectAddSameCardToHandOnReveal.cs
+++ Assets/_MyProject/Scripts/Gameplay/Card/CardSpecialEffects/Effects/CardEffectAddSameCardToHandOnReveal.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public class CardEffectAddSameCardToHandOnReveal : CardEffectBase
 {
     public override void Subscribe()
@@ -12,6 +14,9 @@ public class CardEffectAddSameCardToHandOnReveal : CardEffectBase
     {
         CardObject _drawnCard = CardsManager.Instance.CreateCard(cardObject.Details.Id, cardObject.IsMy);
         GameplayPlayer _player = cardObject.IsMy ? GameplayManager.Instance.MyPlayer : GameplayManager.Instance.OpponentPlayer;
+        Debug.Log(GameplayManager.Instance.MyPlayer);
+        Debug.Log(GameplayManager.Instance.OpponentPlayer);
+        Debug.Log(_player);
         if (!_player.IsMy)
         {
             return;
