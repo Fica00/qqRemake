fb2974204f72ee039a4d7de3a06dba9956f66926
Milan <milan.gordic3@hotmail.com>
2024-06-18 11:46:23 +0200

    47_DarkBlade
    Only effect/ability done
    No Sprites

--- Assets/_MyProject/Scripts/Gameplay/Card/CardSpecialEffects/Effects/CardEffectDestroyYourOtherQommonsHereWithPowerGreaterOrEqualX.cs
+++ Assets/_MyProject/Scripts/Gameplay/Card/CardSpecialEffects/Effects/CardEffectDestroyYourOtherQommonsHereWithPowerGreaterOrEqualX.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CardEffectDestroyYourOtherQommonsHereWithPowerGreaterOrEqualX : CardEffectBase
 {
     [SerializeField] private int powerX;
+
     public override void Subscribe()
     {
         if (!GameplayManager.Instance.Lanes[(int)cardObject.LaneLocation].CanRemoveCards())
@@ -26,23 +27,38 @@ public class CardEffectDestroyYourOtherQommonsHereWithPowerGreaterOrEqualX : Car
             return;
         }
 
-        List<CardObject> _myCardsOnLane = GameplayManager.Instance.TableHandler.GetCards(cardObject.IsMy, cardObject.LaneLocation);
+        List<CardObject> _cardsToBeDestroyed = new List<CardObject>();
+        List<CardObject> _oppoentsCardsOnLine = GameplayManager.Instance.TableHandler.GetCards(!cardObject.IsMy, cardObject.LaneLocation);
 
-        if (_myCardsOnLane.Contains(cardObject))
+        if (_oppoentsCardsOnLine.Count == 0)
         {
-            _myCardsOnLane.Remove(cardObject);
+            return;
         }
 
+        foreach (var _cardOnLane in _oppoentsCardsOnLine)
+        {
+            if (_cardOnLane.Stats.Power >= powerX) 
+            {
+                _cardsToBeDestroyed.Add(_cardOnLane);
+                Debug.Log("!!!!!!!!!!!!!!!!!!Add card to destroy!!!!!!!!!!!!!!!!!!!");
+            }
+        }
+
+        if (_cardsToBeDestroyed.Count == 0)
+        {
+            Debug.Log("!!!!!!!!!!!!!!!!!!No cards to destroy!!!!!!!!!!!!!!!!!!!");
+            return;
+        }
+        
         if (GameplayManager.IsPvpGame)
         {
-            ((GameplayManagerPVP)GameplayManager.Instance).TellOpponentToDestroyCardsOnTable(_myCardsOnLane, true);
+            ((GameplayManagerPVP)GameplayManager.Instance).TellOpponentToDestroyCardsOnTable(_cardsToBeDestroyed, false);
         }
 
-        GameplayPlayer _myPlayer = cardObject.IsMy ? GameplayManager.Instance.MyPlayer : GameplayManager.Instance.OpponentPlayer;
-        foreach (var _card in _myCardsOnLane.ToList())
+        GameplayPlayer _opponentPlayer = cardObject.IsMy ? GameplayManager.Instance.OpponentPlayer : GameplayManager.Instance.MyPlayer;
+        foreach (var _card in _cardsToBeDestroyed)
         {
-            if(_card.Stats.Power >= powerX)
-                _myPlayer.DestroyCardFromTable(_card);
+            _opponentPlayer.DestroyCardFromTable(_card);
         }
     }
 }
