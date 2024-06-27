7345093d5318d8e6b2b097b73a3ab75f2f05939c
Milan <milan.gordic3@hotmail.com>
2024-06-27 13:41:52 +0200

    Revert

--- Assets/_MyProject/Scripts/Gameplay/Card/CardSpecialEffects/Effects/CardEffectDestroyEnemysNRandomXCostQommonsHere.cs
+++ Assets/_MyProject/Scripts/Gameplay/Card/CardSpecialEffects/Effects/CardEffectDestroyEnemysNRandomXCostQommonsHere.cs
@@ -59,7 +59,7 @@ public class CardEffectDestroyEnemysNRandomXCostQommonsHere : CardEffectBase
 
         if (GameplayManager.IsPvpGame)
         {
-            var _placeId = GameplayManager.Instance.LaneIdForQoomonsToDestroy(_cardsToBeDestroyed, true);
+            var _placeId = GameplayManager.Instance.LaneIdForQoomonsToDestroy(_cardsToBeDestroyed, false);
             GameplayManager.Instance.DestroyCardsOnTable(_placeId);
         }
         else
