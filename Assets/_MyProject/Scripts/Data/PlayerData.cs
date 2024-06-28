6926045df87f649dd046f6b5b3f61dacfc6ea1cb
Filip <filpkrcmarevic555@gmail.com>
2024-06-28 17:29:07 +0200

    Added new levels

--- Assets/_MyProject/Scripts/Data/PlayerData.cs
+++ Assets/_MyProject/Scripts/Data/PlayerData.cs
@@ -371,7 +371,6 @@ public class PlayerData
 
             int _currentLevelXp = Level == 0 ? exp : Exp - GetXpForLevel(Level - 1);
             int _nextLevelXp = GetXpForNextLevel();
-            Debug.Log($"{_currentLevelXp} -> {_nextLevelXp} => { (float)_currentLevelXp / _nextLevelXp}");
             return (float)_currentLevelXp / _nextLevelXp;
         }
     }
@@ -418,7 +417,6 @@ public class PlayerData
         {
             return 1;
         }
-        Debug.Log($"exp needed for level: {_level} -> {DataManager.Instance.GameData.ExpBorders[_level]}");
         return DataManager.Instance.GameData.ExpBorders[_level];
     }
 
